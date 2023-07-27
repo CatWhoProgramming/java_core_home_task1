eb488ce305b2bd448bd85130ac09aa50bf95f8ae
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-27 17:38:26 +0200

    change name of array

--- task1/src/Task4/Computer.java
+++ task1/src/Task4/Computer.java
@@ -1,5 +1,13 @@
 package Task4;
 
 public class Computer {
-    
+    private Computer[] computers = new Computer[5];
+
+    public Computer[] getComputers() {
+        return computers;
+    }
+
+    public void setComputers(Computer[] computers) {
+        this.computers = computers;
+    }
 }
