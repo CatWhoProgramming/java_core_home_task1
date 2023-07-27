b5e4375eeb64845f290d51c2d60e57dffab0ca4b
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-27 17:32:46 +0200

    Create class address

---
+++ task1/src/Task5/Address.java
@@ -0,0 +1,58 @@
+package Task5;
+
+public class Address {
+    private int index = 10;
+    private String country = "Ukraine";
+    private String city = "Kyiv";
+    private String street = "Central";
+    private  int house = 34;
+    private int apartment = 12;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+
+    public int getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(int apartment) {
+        this.apartment = apartment;
+    }
+}
