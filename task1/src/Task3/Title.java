3c8021bbeb5a819591163e0d0e78e3c0baa1048a
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-27 13:22:52 +0200

    Wrote class Title

---
+++ task1/src/Task3/Title.java
@@ -0,0 +1,13 @@
+package Task3;
+
+public class Title {
+    private String title = " Harry Potter and the Philosopher's Stone";
+
+    void show() {
+        System.out.println(title);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
