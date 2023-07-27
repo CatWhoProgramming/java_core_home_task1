f7d2fbbd8f868c7585554c3e45a81d01b5b41582
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-27 13:21:53 +0200

    Wrote class Author

---
+++ task1/src/Task3/Author.java
@@ -0,0 +1,13 @@
+package Task3;
+
+public class Author {
+    private String name = "Joanne Rowling";
+
+    void show() {
+        System.out.println(name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
