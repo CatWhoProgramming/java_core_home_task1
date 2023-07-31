428728659223f9699cee73f783e2f8ad83a04147
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-31 09:47:25 +0200

    Change call code Rectangle class

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -6,16 +6,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Rectangle rect = new Rectangle(4, 5);
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter side1 and side2 for class rectangle:");
         double side1 = input.nextDouble();
         double side2 = input.nextDouble();
-        rect.setSide1(side1);
-        rect.setSide2(side2);
-        System.out.println("Enter side1 and side2 for class rectangle:");
-        System.out.println(rect.perimeterCalculator(rect.getSide1(), rect.getSide2()));
-        System.out.println( rect.areaCalculator(rect.getSide1(), rect.getSide2()));
+        Rectangle rect = new Rectangle(side1, side2);
+        System.out.println(rect.perimeterCalculator());
+        System.out.println(rect.areaCalculator());
         input.close();
 
         fillComputerArray();
@@ -50,11 +47,7 @@ public class Main {
 
     private static void fillComputerArray() {
         Computer comp = new Computer();
-        Computer comp1 = new Computer();
-        Computer comp2 = new Computer();
-        Computer comp3 = new Computer();
-        Computer comp4 = new Computer();
-        Computer comp5 = new Computer();
+
         for (int i=0; i<comp.getComputers().length; i++ ){
             String name = "comp"+i;
             //comp.setComputers(1);
